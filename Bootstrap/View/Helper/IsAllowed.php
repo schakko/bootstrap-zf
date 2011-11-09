ee8a87bcf453291f7562f0c03f520cbcadf5319e
Christopher Klein <ckl@ecw.de>
2011-11-09 15:24:04 +0100

    - session user is loaded on every request

--- Bootstrap/View/Helper/IsAllowed.php
+++ Bootstrap/View/Helper/IsAllowed.php
@@ -1,6 +1,4 @@
 <?php
-require_once "Bootstrap/Model/SessionUser.php";
-
 class Bootstrap_View_Helper_IsAllowed extends Zend_View_Helper_Abstract
 {
 	/**
@@ -8,23 +6,15 @@ class Bootstrap_View_Helper_IsAllowed extends Zend_View_Helper_Abstract
 	 */
 	private $_acl = null;
 
-	/**
-	 * @var Bootstrap_Model_SessionUser
-	 */
-	private $_sessionUser = null;
-
 	public function setAcl(Zend_Acl $acl)
 	{
 		$this->_acl = $acl;
 	}
 
-	public function setSessionUser(Bootstrap_Model_SessionUser $sessionUser)
-	{
-		$this->_sessionUser = $sessionUser;
-	}
-
 	public function IsAllowed($resource, $privilege)
 	{
-		return $this->_acl->isAllowed($this->_sessionUser, $resource, $privilege);
+		$session = Zend_Session::namespaceGet('user');
+		$sessionUser = $session['user'];
+		return $this->_acl->isAllowed($sessionUser, $resource, $privilege);
 	}
 }
